5a9b713b16b6c4b13cccadbb3860ffa40177dc9b
Eirik Brandtzæg <eirikb@eirikb.no>
2011-01-13 22:59:05 +0100

    Formatting... Still having issues in prod, not sure where error is

--- spec/BodySpec.js
+++ spec/BodySpec.js
@@ -1,129 +1,130 @@
 describe("Body", function() {
-    var body1;
-    var body2;
-    var body3;
-    var body4;
+	var body1;
+	var body2;
+	var body3;
+	var body4;
 
-    beforeEach( function() {
-        body1 = new OGE.Body(1, 2, 3, 4);
-        body2 = new OGE.Body(3, 4, 2, 1);
-        body3 = new OGE.Body(5, 7);
-        body4 = new OGE.Body();
-    });
+	beforeEach(function() {
+		body1 = new OGE.Body(1, 2, 3, 4);
+		body2 = new OGE.Body(3, 4, 2, 1);
+		body3 = new OGE.Body(5, 7);
+		body4 = new OGE.Body();
+	});
 
-    it("should have public x, y, width and height", function() {
-        expect(body1.x).toEqual(1);
-        expect(body1.y).toEqual(2);
-        expect(body3.x).toEqual(5);
-        expect(body3.y).toEqual(7);
-        expect(body4.x).toEqual(0);
-        expect(body4.y).toEqual(0);
+	it("should have public x, y, width and height", function() {
+		expect(body1.x).toEqual(1);
+		expect(body1.y).toEqual(2);
+		expect(body3.x).toEqual(5);
+		expect(body3.y).toEqual(7);
+		expect(body4.x).toEqual(0);
+		expect(body4.y).toEqual(0);
 
-        body1.x = 100;
-        expect(body1.x).toEqual(100);
+		body1.x = 100;
+		expect(body1.x).toEqual(100);
 
-        expect(body1.width).toEqual(3);
-        expect(body1.height).toEqual(4);
-        expect(body2.width).toEqual(2);
-        expect(body2.height).toEqual(1);
-        expect(body3.width).toEqual(1);
-        expect(body3.height).toEqual(1);
-        expect(body4.width).toEqual(1);
-        expect(body4.height).toEqual(1);
+		expect(body1.width).toEqual(3);
+		expect(body1.height).toEqual(4);
+		expect(body2.width).toEqual(2);
+		expect(body2.height).toEqual(1);
+		expect(body3.width).toEqual(1);
+		expect(body3.height).toEqual(1);
+		expect(body4.width).toEqual(1);
+		expect(body4.height).toEqual(1);
 
-        body1.width = 1000;
-        expect(body1.width).toEqual(1000);
-    });
+		body1.width = 1000;
+		expect(body1.width).toEqual(1000);
+	});
 
-    it("should have speed set to 0", function() {
-        expect(body1.speed).toEqual(0);
-        body1.speed = 1337;
-        expect(body1.speed).toEqual(1337);
-        var speed = 42;
-        body1.speed = speed;
-        expect(body1.speed).toEqual(42);
-        speed = 2;
-        expect(body1.speed).toEqual(42);
-    });
+	it("should have speed set to 0", function() {
+		expect(body1.speed).toEqual(0);
+		body1.speed = 1337;
+		expect(body1.speed).toEqual(1337);
+		var speed = 42;
+		body1.speed = speed;
+		expect(body1.speed).toEqual(42);
+		speed = 2;
+		expect(body1.speed).toEqual(42);
+	});
 
-    it("should direction set to null, but can be set", function() {
-        expect(body1.direction).toEqual(null);
-        expect(body4.direction).toEqual(null);
-        body4.setDirection(0, 1);
-        expect(body4.direction.cos).toEqual(0);
-        body4.setDirection(0, 1);
-        expect(body4.direction.sin).toEqual(1);
-        body4.setDirection(1, 0);
-        expect(body4.direction.cos).toEqual(1);
-        expect(body4.direction.sin).toEqual(0);
-    });
+	it("should direction set to null, but can be set", function() {
+		expect(body1.direction).toEqual(null);
+		expect(body4.direction).toEqual(null);
+		body4.setDirection(0, 1);
+		expect(body4.direction.cos).toEqual(0);
+		body4.setDirection(0, 1);
+		expect(body4.direction.sin).toEqual(1);
+		body4.setDirection(1, 0);
+		expect(body4.direction.cos).toEqual(1);
+		expect(body4.direction.sin).toEqual(0);
+	});
 
-    it("should call events on activating", function() {
-        var count = 0;
-        body1.onActive( function() {
-            expect(body1.isActive()).toBeTruthy();
-            count++;
-        });
+	it("should call events on activating", function() {
+		var count = 0;
+		body1.onActive(function() {
+			expect(body1.isActive()).toBeTruthy();
+			count++;
+		});
 
-        body1.onActive( function() {
-            expect(body1.isActive()).toBeTruthy();
-            count++;
-        });
+		body1.onActive(function() {
+			expect(body1.isActive()).toBeTruthy();
+			count++;
+		});
 
-        body1.onDeactive( function() {
-            expect(body1.isActive()).toBeFalsy();
-            count++;
-        });
+		body1.onDeactive(function() {
+			expect(body1.isActive()).toBeFalsy();
+			count++;
+		});
 
-        body1.onDeactive( function() {
-            expect(body1.isActive()).toBeFalsy();
-            count++;
-        });
+		body1.onDeactive(function() {
+			expect(body1.isActive()).toBeFalsy();
+			count++;
+		});
 
-        // Setting the same state will not trigger the events
-        body1.setActive(false);
-        body1.setActive(true);
-        body1.setActive(true);
-        body1.setActive(false);
-        body1.setActive(false);
+		// Setting the same state will not trigger the events
+		body1.setActive(false);
+		body1.setActive(true);
+		body1.setActive(true);
+		body1.setActive(false);
+		body1.setActive(false);
 
-        expect(count).toEqual(4);
-    });
+		expect(count).toEqual(4);
+	});
 
-    it("should call events on collision", function() {
-        var count = 0;
-        body1.onCollision( function() {
-            count++;
-        }) ;
+	it("should call events on collision", function() {
+		var count = 0;
+		body1.onCollision(function() {
+			count++;
+		});
 
-        body1.onCollision( function() {
-            count++;
-        });
+		body1.onCollision(function() {
+			count++;
+		});
 
-        body1.collide();
-        body1.collide();
-        expect(count).toEqual(4);
-    });
+		body1.collide();
+		body1.collide();
+		expect(count).toEqual(4);
+	});
 
-    it("should check collision on both coordinates + size as well as body object", function() {
-        var b1 = new OGE.Body(0, 0, 10, 10);
-        var b2 = new OGE.Body(5, 5, 10, 10);
-        var b3 = new OGE.Body(10, 10, 10, 10);
-        expect(b1.intersects(b2)).toBeTruthy();
-        expect(b1.intersects(5, 5, 10, 10)).toBeTruthy();
-        expect(b2.intersects(b1)).toBeTruthy();
-        expect(b1.intersects(b3)).toBeFalsy();
-        expect(b3.intersects(b2)).toBeTruthy();
-        expect(b3.intersects(b1)).toBeFalsy();
-        expect(b3.intersects(0, 0, 10, 10)).toBeFalsy();
-    });
+	it("should check collision on both coordinates + size as well as body object", function() {
+		var b1 = new OGE.Body(0, 0, 10, 10);
+		var b2 = new OGE.Body(5, 5, 10, 10);
+		var b3 = new OGE.Body(10, 10, 10, 10);
+		expect(b1.intersects(b2)).toBeTruthy();
+		expect(b1.intersects(5, 5, 10, 10)).toBeTruthy();
+		expect(b2.intersects(b1)).toBeTruthy();
+		expect(b1.intersects(b3)).toBeFalsy();
+		expect(b3.intersects(b2)).toBeTruthy();
+		expect(b3.intersects(b1)).toBeFalsy();
+		expect(b3.intersects(0, 0, 10, 10)).toBeFalsy();
+	});
 
-    it("should be possible to calculate the intersection size bwtween bodies", function() {
-        var b1 = new OGE.Body(0, 0, 10, 10);
-        expect(b1.intersection(9, 0, 10, 10)).toBe(10);
-        expect(b1.intersection(8, 0, 10, 11)).toBe(20);
-        expect(b1.intersection(7, 0, 2, 14)).toBe(20);
-        expect(b1.intersection(5, 5, 5, 5)).toBe(25);
-    });
+	it("should be possible to calculate the intersection size bwtween bodies", function() {
+		var b1 = new OGE.Body(0, 0, 10, 10);
+		expect(b1.intersection(9, 0, 10, 10)).toBe(10);
+		expect(b1.intersection(8, 0, 10, 11)).toBe(20);
+		expect(b1.intersection(7, 0, 2, 14)).toBe(20);
+		expect(b1.intersection(5, 5, 5, 5)).toBe(25);
+	});
 
 });
+
--- spec/DirectionSpec.js
+++ spec/DirectionSpec.js
@@ -1,47 +1,48 @@
 describe("Direction", function() {
-    var direction1;
+	var direction1;
 
-    beforeEach( function() {
-        d1 = new OGE.Direction(1, 2);
-    });
+	beforeEach(function() {
+		d1 = new OGE.Direction(1, 2);
+	});
 
-    it("should create cos and sin based on coordinates", function() {
-        var d = OGE.Direction.create(0, 0, 0, 1);
-        expect(d).toBeDefined();
-        expect(d.cos).toEqual(0);
-        expect(d.sin).toEqual(1);
-        d = OGE.Direction.create(0, 0, 1, 0);
-        expect(d.cos).toEqual(1);
-        expect(d.sin).toEqual(0);
-    });
+	it("should create cos and sin based on coordinates", function() {
+		var d = OGE.Direction.create(0, 0, 0, 1);
+		expect(d).toBeDefined();
+		expect(d.cos).toEqual(0);
+		expect(d.sin).toEqual(1);
+		d = OGE.Direction.create(0, 0, 1, 0);
+		expect(d.cos).toEqual(1);
+		expect(d.sin).toEqual(0);
+	});
 
-    it("should be possible to clone a direction", function() {
-        expect(d1.cos).toBe(1);
-        expect(d1.sin).toBe(2);
-        var d2 = d1;
-        var d3 = d1.clone();
-        d2.cos = 7;
-        d2.sin = 42;
-        d3.cos = 4;
-        d3.sin = 5;
-        expect(d1.cos).toBe(7);
-        expect(d1.sin).toBe(42);
-        expect(d2.cos).toBe(7);
-        expect(d2.sin).toBe(42);
-        expect(d3.cos).toBe(4);
-        expect(d3.sin).toBe(5);
-    });
+	it("should be possible to clone a direction", function() {
+		expect(d1.cos).toBe(1);
+		expect(d1.sin).toBe(2);
+		var d2 = d1;
+		var d3 = d1.clone();
+		d2.cos = 7;
+		d2.sin = 42;
+		d3.cos = 4;
+		d3.sin = 5;
+		expect(d1.cos).toBe(7);
+		expect(d1.sin).toBe(42);
+		expect(d2.cos).toBe(7);
+		expect(d2.sin).toBe(42);
+		expect(d3.cos).toBe(4);
+		expect(d3.sin).toBe(5);
+	});
 
-    it("should be possible to rotate a direction", function() {
-        var d = new OGE.Direction(1, 0);
-        d.rotate(90);
-        expect(d.cos).toBeGreaterThan(-0.0001);
-        expect(d.cos).toBeLessThan(0.0001);
-        expect(d.sin).toBe(1);
-        d.rotate(-45);
-        expect(d.cos).toBeGreaterThan(0.707);
-        expect(d.cos).toBeLessThan(0.708);
-        expect(d.sin).toBeGreaterThan(0.707)
-        expect(d.sin).toBeLessThan(0.708);
-    });
+	it("should be possible to rotate a direction", function() {
+		var d = new OGE.Direction(1, 0);
+		d.rotate(90);
+		expect(d.cos).toBeGreaterThan( - 0.0001);
+		expect(d.cos).toBeLessThan(0.0001);
+		expect(d.sin).toBe(1);
+		d.rotate( - 45);
+		expect(d.cos).toBeGreaterThan(0.707);
+		expect(d.cos).toBeLessThan(0.708);
+		expect(d.sin).toBeGreaterThan(0.707)
+		expect(d.sin).toBeLessThan(0.708);
+	});
 });
+
