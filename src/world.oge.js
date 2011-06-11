3d46b1964a4c5da9633ed7ddbef235f386fb1dc3
Eirik Brandtzæg <eirikb@eirikb.no>
2011-06-11 13:54:48 +0200

    Fix for sliding against walls

--- src/world.oge.js
+++ src/world.oge.js
@@ -8,23 +8,23 @@
  * @return {OGE.World}
  */
 OGE.World = function(width, height, zoneSize) {
-	this.width = typeof(width) != 'undefined' ? width: 640;
-	this.height = typeof(height) != 'undefined' ? height: 480;
-	this.zoneSize = typeof(zoneSize) != 'undefined' ? zoneSize: 10;
-	this.activeBodies = [];
+    this.width = typeof(width) != 'undefined' ? width: 640;
+    this.height = typeof(height) != 'undefined' ? height: 480;
+    this.zoneSize = typeof(zoneSize) != 'undefined' ? zoneSize: 10;
+    this.activeBodies = [];
 
-	var xZones = width / this.zoneSize + 1 << 0;
-	var yZones = height / this.zoneSize + 1 << 0;
-	this.zones = [];
+    var xZones = width / this.zoneSize + 1 << 0;
+    var yZones = height / this.zoneSize + 1 << 0;
+    this.zones = [];
 
-	for (var x = 0; x < xZones; x++) {
-		this.zones[x] = [];
-		for (var y = 0; y < yZones; y++) {
-			this.zones[x][y] = new OGE.Zone(x, y);
-		}
-	}
+    for (var x = 0; x < xZones; x++) {
+        this.zones[x] = [];
+        for (var y = 0; y < yZones; y++) {
+            this.zones[x][y] = new OGE.Zone(x, y);
+        }
+    }
 
-	// Private functions
+    // Private functions
 };
 
 /**
@@ -36,19 +36,19 @@ OGE.World = function(width, height, zoneSize) {
  * @return true if body was added, false if not (out of bounds)
  */
 OGE.World.prototype.addBody = function(body, active) {
-	if (this.addBodyToZones(body) !== true) {
-		return false;
-	}
+    if (this.addBodyToZones(body) !== true) {
+        return false;
+    }
 
-	if (arguments.length >= 2) {
-		body.active = active;
-	}
+    if (arguments.length >= 2) {
+        body.active = active;
+    }
 
-	if (body.active) {
-		this.activeBodies.push(body);
-	}
+    if (body.active) {
+        this.activeBodies.push(body);
+    }
 
-	return true;
+    return true;
 };
 
 /**
@@ -58,13 +58,13 @@ OGE.World.prototype.addBody = function(body, active) {
  * @param {OGE.Body} body Body to remove
  */
 OGE.World.prototype.removeBody = function(body) {
-	this.removeBodyFromZones(body);
-	for (var i = 0; i < this.activeBodies.length; i++) {
-		if (this.activeBodies[i] === body) {
-			this.activeBodies.splice(i, 1);
-			break;
-		}
-	}
+    this.removeBodyFromZones(body);
+    for (var i = 0; i < this.activeBodies.length; i++) {
+        if (this.activeBodies[i] === body) {
+            this.activeBodies.splice(i, 1);
+            break;
+        }
+    }
 
 };
 
@@ -80,39 +80,39 @@ OGE.World.prototype.removeBody = function(body) {
  * @return Array of OGE.Body found, including the given body
  */
 OGE.World.prototype.getBodies = function(bodyOrX, y, width, height) {
-	var body, x;
-	body = x = bodyOrX;
-	if (arguments.length === 1) {
-		x = body.x;
-		y = body.y;
-		width = body.width;
-		height = body.height;
-	}
-	x = x < 0 ? 0: x;
-	x = x + width > this.width ? this.width - width: x;
-	y = y < 0 ? 0: y;
-	y = y + height > this.height ? this.height - height: y;
+    var body, x;
+    body = x = bodyOrX;
+    if (arguments.length === 1) {
+        x = body.x;
+        y = body.y;
+        width = body.width;
+        height = body.height;
+    }
+    x = x < 0 ? 0: x;
+    x = x + width > this.width ? this.width - width: x;
+    y = y < 0 ? 0: y;
+    y = y + height > this.height ? this.height - height: y;
 
-	var bodies = [];
-	var zones = this.getZones(x, y, width, height);
-	for (var i = 0; i < zones.length; i++) {
-		var bodies2 = zones[i].bodies;
-		for (var j = 0; j < bodies2.length; j++) {
-			var b = bodies2[j];
-			var contains = false;
-			for (var k = 0; k < bodies.length; k++) {
-				if (bodies[k] === b) {
-					contains = true;
-					break;
-				}
-			}
-			if (!contains) {
-				bodies.push(b);
-			}
-		}
-	}
+    var bodies = [];
+    var zones = this.getZones(x, y, width, height);
+    for (var i = 0; i < zones.length; i++) {
+        var bodies2 = zones[i].bodies;
+        for (var j = 0; j < bodies2.length; j++) {
+            var b = bodies2[j];
+            var contains = false;
+            for (var k = 0; k < bodies.length; k++) {
+                if (bodies[k] === b) {
+                    contains = true;
+                    break;
+                }
+            }
+            if (!contains) {
+                bodies.push(b);
+            }
+        }
+    }
 
-	return bodies;
+    return bodies;
 };
 
 /**
@@ -127,32 +127,32 @@ OGE.World.prototype.getBodies = function(bodyOrX, y, width, height) {
  * @return Array of OGE.Zone found, including the given body
  */
 OGE.World.prototype.getZones = function(bodyOrX, y, width, height) {
-	var body, x;
-	body = x = bodyOrX;
-	if (arguments.length === 1) {
-		x = body.x;
-		y = body.y;
-		width = body.width;
-		height = body.height;
-	}
+    var body, x;
+    body = x = bodyOrX;
+    if (arguments.length === 1) {
+        x = body.x;
+        y = body.y;
+        width = body.width;
+        height = body.height;
+    }
 
-	if (x >= 0 && x + width - 1 < this.width && y >= 0 && y + height - 1 < this.height) {
-		var x1 = x / this.zoneSize << 0;
-		var x2 = (x + width) / this.zoneSize << 0;
-		var y1 = y / this.zoneSize << 0;
-		var y2 = (y + height) / this.zoneSize << 0;
+    if (x >= 0 && x + width - 1 < this.width && y >= 0 && y + height - 1 < this.height) {
+        var x1 = x / this.zoneSize << 0;
+        var x2 = (x + width) / this.zoneSize << 0;
+        var y1 = y / this.zoneSize << 0;
+        var y2 = (y + height) / this.zoneSize << 0;
 
-		var pos = 0;
-		var z = [];
-		for (x = x1; x <= x2; x++) {
-			for (y = y1; y <= y2; y++) {
-				z[pos++] = this.zones[x][y];
-			}
-		}
-		return z;
-	} else {
-		return [];
-	}
+        var pos = 0;
+        var z = [];
+        for (x = x1; x <= x2; x++) {
+            for (y = y1; y <= y2; y++) {
+                z[pos++] = this.zones[x][y];
+            }
+        }
+        return z;
+    } else {
+        return [];
+    }
 };
 
 /**
@@ -162,129 +162,127 @@ OGE.World.prototype.getZones = function(bodyOrX, y, width, height) {
  * @param {number} steps Amount of steps to do
  */
 OGE.World.prototype.step = function(steps) {
-	steps = arguments.length === 0 ? 1: steps;
-	for (var step = 0; step < steps; step++) {
-		for (var i = 0; i < this.activeBodies.length; i++) {
-			var body = this.activeBodies[i];
-			if (body.speed > 0 && body.direction !== null) {
-				this.moveBody(body);
-			}
-		}
-	}
+    steps = arguments.length === 0 ? 1: steps;
+    for (var step = 0; step < steps; step++) {
+        for (var i = 0; i < this.activeBodies.length; i++) {
+            var body = this.activeBodies[i];
+            if (body.speed > 0 && body.direction !== null) {
+                this.moveBody(body);
+            }
+        }
+    }
 };
 
 // Originally private methods
 OGE.World.prototype.addBodyToZones = function(body) {
-	var zones = this.getZones(body);
-	if (zones.length === 0) {
-		return false;
-	}
-	for (var i = 0; i < zones.length; i++) {
-		zones[i].addBody(body);
-	}
-	return true;
+    var zones = this.getZones(body);
+    if (zones.length === 0) {
+        return false;
+    }
+    for (var i = 0; i < zones.length; i++) {
+        zones[i].addBody(body);
+    }
+    return true;
 };
 
 OGE.World.prototype.removeBodyFromZones = function(body) {
-	var zones = this.getZones(body);
-	for (var i = 0; i < zones.length; i++) {
-		zones[i].removeBody(body);
-	}
+    var zones = this.getZones(body);
+    for (var i = 0; i < zones.length; i++) {
+        zones[i].removeBody(body);
+    }
 };
 
 OGE.World.prototype.moveBody = function(body, direction, steps) {
-	var lastX, lastY, bodies;
-	if (arguments.length === 1) {
-		direction = body.direction;
-		steps = body.speed;
-	}
+    var lastX, lastY, bodies;
+    if (arguments.length === 1) {
+        direction = body.direction;
+        steps = body.speed;
+    }
 
-	for (var i = 0; i < steps; i++) {
-		lastX = body.x;
-		lastY = body.y;
-		this.removeBodyFromZones(body);
-		body.x += direction.cos;
-		body.y += direction.sin;
-		if (body.x >= 0 && body.y >= 0 && body.x + body.width <= this.width && body.y + body.height <= this.height) {
-			bodies = this.getBodies(body);
-			for (var j = 0; j < bodies.length; j++) {
-				var body2 = bodies[j];
-				if (body !== body2 && ! body2.intersects(lastX, lastY, body.width, body.height) && body2.intersects(body)) {
-					var collide1 = body.collide(body2) === true;
-					var collide2 = body2.collide(body) === true;
-					if (collide1 && collide2) {
-						body.x = lastX;
-						body.y = lastY;
-						this.addBodyToZones(body);
-						if (body.slide && j >= 0) {
-							this.slideBody(body, direction);
-						} else {
-							return;
-						}
-					}
-				}
-			}
-			this.addBodyToZones(body);
-		} else {
-			body.x = lastX;
-			body.y = lastY;
-			break;
-		}
-	}
+    for (var i = 0; i < steps; i++) {
+        lastX = body.x;
+        lastY = body.y;
+        this.removeBodyFromZones(body);
+        body.x += direction.cos;
+        body.y += direction.sin;
+        body.x = body.x < 0 ? 0: body.x;
+        body.x = body.x + body.width > this.width ? this.width - body.width: body.x;
+        body.y = body.y < 0 ? 0: body.y;
+        body.y = body.y + body.height > this.height ? this.height - body.height: body.y;
+        bodies = this.getBodies(body);
+        for (var j = 0; j < bodies.length; j++) {
+            var body2 = bodies[j];
+            if (body !== body2 && ! body2.intersects(lastX, lastY, body.width, body.height) && body2.intersects(body)) {
+                var collide1 = body.collide(body2) === true;
+                var collide2 = body2.collide(body) === true;
+                if (collide1 && collide2) {
+                    body.x = lastX;
+                    body.y = lastY;
+                    this.addBodyToZones(body);
+                    if (body.slide && j >= 0) {
+                        this.slideBody(body, direction);
+                    } else {
+                        return;
+                    }
+                }
+            }
+        }
+        this.addBodyToZones(body);
+    }
 };
 
 OGE.World.prototype.slideBody = function(body, direction) {
-	var self = this;
-	var getIntersection = function(direction) {
-		var ignoreBodies = [],
-		bodies = self.getBodies(body),
-		intersection,
-		i,
-		j,
-		x,
-		y,
-		body2,
-		ignore;
-		for (i = 0; i < bodies.length; i++) {
-			body2 = bodies[i];
-			if (body2 !== body && body2.intersects(body)) {
-				ignoreBodies.push(body2);
-			}
-		}
-		intersection = 0;
-		x = body.x + direction.cos * 1.9 << 0;
-		if (x !== body.x) {
-			x = body.x + (x > body.x ? 1: - 1);
-		}
-		y = body.y + direction.sin * 1.9 << 0;
-		if (y !== body.y) {
-			y = body.y + (y > body.y ? 1: - 1);
-		}
-		bodies = self.getBodies(x, y, body.width, body.height);
-		for (i = 0; i < bodies.length; i++) {
-			body2 = bodies[i];
-			if (body2 !== body && body2.intersects(x, y, body.width, body.height)) {
-				ignore = false;
-				for (j = 0; j < ignoreBodies.length; j++) {
-					if (body2 === ignoreBodies[j]) {
-						ignore = true;
-						break;
-					}
-				}
-				if (!ignore) {
-					intersection += body2.intersection(x, y, body.width, body.height);
-				}
-			}
-		}
-		return intersection << 0;
-	};
+    var self = this;
+    var getIntersection = function(direction) {
+        var ignoreBodies = [],
+        bodies = self.getBodies(body),
+        intersection,
+        i,
+        j,
+        x,
+        y,
+        body2,
+        ignore;
+        for (i = 0; i < bodies.length; i++) {
+            body2 = bodies[i];
+            if (body2 !== body && body2.intersects(body)) {
+                ignoreBodies.push(body2);
+            }
+        }
+        intersection = 0;
+        x = body.x + direction.cos * 1.9 << 0;
+        if (x !== body.x) {
+            x = body.x + (x > body.x ? 1: - 1);
+        }
+        y = body.y + direction.sin * 1.9 << 0;
+        if (y !== body.y) {
+            y = body.y + (y > body.y ? 1: - 1);
+        }
+        bodies = self.getBodies(x, y, body.width, body.height);
+        for (i = 0; i < bodies.length; i++) {
+            body2 = bodies[i];
+            if (body2 !== body && body2.intersects(x, y, body.width, body.height)) {
+                ignore = false;
+                for (j = 0; j < ignoreBodies.length; j++) {
+                    if (body2 === ignoreBodies[j]) {
+                        ignore = true;
+                        break;
+                    }
+                }
+                if (!ignore) {
+                    intersection += body2.intersection(x, y, body.width, body.height);
+                }
+            }
+        }
+        return intersection << 0;
+    };
 
-	var intersection1 = getIntersection(direction.clone().rotate( - 45));
-	var intersection2 = getIntersection(direction.clone().rotate(45));
-	if (intersection1 < intersection2) {
-		this.moveBody(body, direction.clone().rotate( - 90), 1);
-	} else if (intersection1 > intersection2) {
-		this.moveBody(body, direction.clone().rotate(90), 1);
-	}
+    var intersection1 = getIntersection(direction.clone().rotate( - 45));
+    var intersection2 = getIntersection(direction.clone().rotate(45));
+    if (intersection1 < intersection2) {
+        this.moveBody(body, direction.clone().rotate( - 90), 1);
+    } else if (intersection1 > intersection2) {
+        this.moveBody(body, direction.clone().rotate(90), 1);
+    }
 };
 
